d5d050e732cc81c1b90944f09c7d0fb565b8662b
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-05-06 15:45:10 +0300

    init: template with webpack and typescripte

---
+++ base_projects/src/js/main.js
@@ -0,0 +1,8 @@
+import '../css/main.css';
+import '../scss/main.scss';
+import '../index.html';
+
+'use strict()';
+
+window.addEventListener('DOMContentLoaded', () => {
+});
---
+++ base_projects/webpack.config.js
@@ -0,0 +1,111 @@
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable comma-dangle */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './js/main.js',
+  output: {
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'app'),
+    publicPath: ''
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'app'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'app/assets')}
+    //   ]
+    // }),
+  ],
+  devtool: isProd ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+          test: /\.css$/i,
+          use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // options: {
+            //   hmr: isDev
+            // },
+          },
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + '/';
+              },
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: `./img/${filename('[ext]')}`
+          }
+        }],
+      },
+      {
+        test: /\.(?:|woff|wolf2)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: `./fonts/${filename('[ext]')}`
+          }
+        }],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ]
+  }
+};
